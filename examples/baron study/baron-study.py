108d0dab52435b696ff82c99f05833d70feb1f6b
dan-slater <danielgslater+1@gmail.com>
2022-10-19 18:53:12 +0200

    Working on repr curves.

--- examples/baron study/baron-study.py
+++ examples/baron study/baron-study.py
@@ -1,4 +1,5 @@
 """Module for examnple study of baron data."""
+import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
 
@@ -9,36 +10,12 @@ from paramaterial.plug import DataSet, DataItem
 def main():
     """Main function."""
 
-    raw_dataset = DataSet(data_dir='data/01 prepared data', info_path='info/01 prepared info.xlsx')
-    dataitem = raw_dataset[{'rate': [1], 'material': ['H560']}][0]
-    # make pairplot from dataitem.data, color by temperature
-    print(dataitem.data[::100])
-    sns.pairplot(dataitem.data[::1000])
-    plt.show()
+    dataset = DataSet('data/02 processed data', 'info/02 processed info.xlsx')
+    dataset.make_representative_curves('data/03 repr data', 'info/03 repr info.xlsx',
+                                       ['material', 'rate', 'temperature'],
+                                       interp_res=100,
+                                       interp_by='Strain')
 
-
-# remove the trailing data in the dataitem.data after the maximimum Force
-def remove_trailing_data(dataitem: DataItem):
-    max_force = dataitem.data['Force'].max()
-    max_force_index = dataitem.data[dataitem.data['Force'] == max_force].index[0]
-    dataitem.data = dataitem.data[:max_force_index]
-    return dataitem
-
-
-def dataset_plot(dataset: DataSet):
-    pam.plotting.dataset_plot(dataset, x='Strain', y='Stress(MPa)', ylabel='Stress (MPa)', cbar_by='temperature',
-                              cbar_label=r'Temperature ($^{\circ}$C)', style_by='material', alpha=0.8,
-                              xlim=(-0.2, 1.5), grid=True, style_by_label='Material', width_by='rate', width_by_label='Rate (/s)')
-
-
-def dataset_subplot(dataset: DataSet):
-    pam.plotting.dataset_subplots(dataset, x='Strain', y='Stress(MPa)', ylabel='Stress (MPa)',
-                                  nrows=3, ncols=4,
-                                  rows_by='material', cols_by='rate',
-                                  row_keys=[['AC'],['H560'],['H580']], col_keys=[[1],[10],[50],[100]],
-                                  col_titles=['1 s$^{-1}$', '10 s$^{-1}$', '50 s$^{-1}$', '100 s$^{-1}$'],
-                                  cbar_by='temperature', cbar_label='Temperature (C)', grid=True)
-    plt.show()
 
 if __name__ == '__main__':
     main()
--- paramaterial/plug.py
+++ paramaterial/plug.py
@@ -5,15 +5,23 @@ from collections import namedtuple
 from dataclasses import dataclass
 from typing import Dict, Callable, List, Any, Union
 
+import numpy as np
 import pandas as pd
 from tqdm import tqdm
-
 
 @dataclass
 class DataItem:
     test_id: str = None
     data: pd.DataFrame = None
     info: pd.Series = None
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr__(self):
+        """Return a string with the test id, the info Series and the data DataFrame."""
+        # make a more informative string
+        return f'DataItem test_id:{self.test_id}\nDataItem info:\n{self.info}\nDataItem data:\n{self.data}'
 
     @staticmethod
     def read_from_csv(file_path: str):
@@ -25,27 +33,25 @@ class DataItem:
         self.info = info_table.loc[info_table['test id'] == self.test_id].squeeze()
         return self
 
-    def write_to_csv(self, output_dir: str):
+    def write_data_to_csv(self, output_dir: str):
         output_path = output_dir + '/' + self.test_id + '.csv'
         self.data.to_csv(output_path, index=False)
         return self
-
-    def __contains__(self, other: 'DataItem'):
-        if self.test_id != other.test_id:
-            return False
-        if other.data is not None and other.data not in self.data:
-            return False
-        if other.info is not None and other.info not in self.info:
-            return False
-        return True
 
 
 @dataclass
 class DataSet:
     data_dir: str = None
     info_path: str = None
+    datamap: map = None
+    info_table: pd.DataFrame = None
 
     def __init__(self, data_dir: str, info_path: str):
+        """Initialize the dataset.
+        Args:
+            data_dir: The directory containing the data.
+            info_path: The path to the info table.
+        """
         self.data_dir = data_dir
         self.info_path = info_path
         self.info_table = pd.read_excel(self.info_path)
@@ -55,11 +61,80 @@ class DataSet:
         self.datamap = map(lambda path: DataItem.read_from_csv(path), file_paths)
         self.datamap = map(lambda obj: DataItem.get_row_from_info_table(obj, self.info_table), self.datamap)
 
+    def apply_function(self, func: Callable[[DataItem], DataItem]) -> 'DataSet':
+        """Apply a processing function to the dataset."""
+
+        def wrapped_func(dataitem: DataItem):
+            try:
+                dataitem = func(dataitem)
+                dataitem.data.reset_index(drop=True, inplace=True)
+                return dataitem
+            except Exception as e:
+                print(f'Error applying "{func.__name__}": {e}')
+                return dataitem
+
+        new_dataset = self.copy()
+        new_dataset.datamap = map(wrapped_func, new_dataset.datamap)
+        return new_dataset
+
+    def make_representative_curves(self, data_dir: str, info_path: str, filter_keys: List[str], interp_by: str, interp_res: int = 200):
+        """Make representative curves of the dataset and save them to a directory.
+        Args:
+
+        """
+        subset_filters = []
+        for col in filter_keys:
+            values = self.info_table[col].unique()
+            for value in values:
+                subset_filters.append({col: [value]})
+        repr_ids = [f'repr_id_{i:0>4}' for i in range(len(subset_filters))]
+        for repr_id, subset_filter in zip(repr_ids, subset_filters):
+            repr_df = pd.DataFrame()
+            repr_subset = self[subset_filter]
+            for di in repr_subset:
+                interp_vec = np.linspace(di.data[interp_by].min(), di.data[interp_by].max(), interp_res)
+                interp_df = pd.DataFrame({f'interp_vec_{interp_by}': interp_vec})
+                for col in di.data.columns:
+                    if col != interp_by:
+                        interp_col = np.interp(interp_vec, di.data[interp_by], di.data[col])
+                        interp_df[f'interp_{col}'] = interp_col
+                repr_df = pd.concat([repr_df, interp_df])
+            repr_df = repr_df.groupby(f'interp_vec_{interp_by}').mean().reset_index()
+            repr_df.to_csv(data_dir + '/' + repr_id + '.csv', index=False)
+
+    def write_output(self, data_dir: str, info_path: str) -> None:
+        """Execute the processing operations and write the output of the dataset to a directory.
+        Args:
+            data_dir: The directory to write the data to.
+            info_path: The path to write the info table to.
+        """
+        if not os.path.exists(data_dir):
+            os.makedirs(data_dir)
+        for dataitem in self:
+            dataitem.write_data_to_csv(data_dir)
+        self.info_table.to_excel(info_path, index=False)
+        loading_bar = tqdm(range(len(self.info_table)))
+        out_info_table = pd.DataFrame()
+        for i, dataitem in enumerate(copy.deepcopy(self.datamap)):
+            loading_bar.update()
+            dataitem.write_data_to_csv(data_dir)
+            out_info_table = pd.concat([out_info_table, dataitem.info.to_frame().T], ignore_index=True)
+            out_info_table.to_excel(info_path, index=False)
+        loading_bar.close()
+
+    def copy(self) -> 'DataSet':
+        """Return a copy of the dataset."""
+        return copy.deepcopy(self)
+
     def __iter__(self):
         """Iterate over the dataset."""
-        for dataitem in copy.deepcopy(self.datamap):
+        for dataitem in tqdm(copy.deepcopy(self.datamap)):
             if dataitem.test_id in self.info_table['test id'].values:
                 yield dataitem
+
+    def __eq__(self, other):
+        """Check if the datamaps and info tables of the datasets are equal."""
+        return self.datamap == other.datamap and self.info_table.equals(other.info_table)
 
     def __len__(self):
         """Get the number of dataitems in the dataset."""
@@ -67,9 +142,13 @@ class DataSet:
             raise ValueError('Length of info table and datamap are different.')
         return len(self.info_table)
 
-    def __contains__(self, item: DataItem):
-        """Check if a dataitem is in the dataset."""
-        return item.test_id in self.info_table['test id'].values
+    def __repr__(self):
+        """Get a string with the info table head and column headers of the first dataitem."""
+        return f'DataSet:\n' \
+               f'info_table:\n' \
+               f'rows={len(self.info_table)}\n' \
+               f'head=\n{self.info_table.head().to_string()}\n' \
+               f'First {self[0]}'
 
     def __getitem__(self, item: Union[Dict[str, List[Any]], int, slice]) -> Union['DataSet', DataItem]:
         """Get a subset of the dataset using a dictionary of column names and lists of values or using normal list
@@ -77,7 +156,7 @@ class DataSet:
         if isinstance(item, int):
             # get the test id of the ith row in the info table
             test_id = self.info_table.iloc[item]['test id']
-            data = pd.read_csv(self.data_dir + '/' + test_id + '.csv')
+            data = list(copy.deepcopy(self.datamap))[item].data
             info = self.info_table.loc[self.info_table['test id'] == test_id].squeeze()
             return DataItem(test_id, data, info)
         elif isinstance(item, slice):
@@ -101,74 +180,3 @@ class DataSet:
             return subset
         else:
             raise ValueError(f'Invalid argument type: {type(item)}')
-
-    def __add__(self, other: 'DataSet') -> 'DataSet':
-        """Combine two datasets into one."""
-        new_dataset = copy.deepcopy(self)
-        new_dataset.datamap = list(self.datamap) + list(other.datamap)
-        # check for duplicate test ids, if any, rename the duplicate test ids in the other dataset
-        test_ids = self.info_table['test id'].values
-        for test_id in other.info_table['test id'].values:
-            if test_id in test_ids:
-                new_test_id = test_id + '_2'
-                other.info_table.loc[other.info_table['test id'] == test_id, 'test id'] = new_test_id
-                other.datamap = map(lambda obj: DataItem(new_test_id, obj.data, obj.info) if obj.test_id == test_id
-                                    else obj, other.datamap)
-        new_dataset.info_table = pd.concat([self.info_table, other.info_table])
-        return new_dataset
-
-    def __sub__(self, other: 'DataSet') -> 'DataSet':
-        """Remove a subset of the dataset."""
-        new_dataset = copy.deepcopy(self)
-        new_dataset.datamap = list(filter(lambda x: x not in other, self.datamap))
-        new_dataset.info_table = self.info_table.loc[~self.info_table['test id'].isin(other.info_table['test id'])]
-        return new_dataset
-
-    def __eq__(self, other: 'DataSet') -> bool:
-        """Check if two datasets are equal."""
-        if self.info_table.equals(other.info_table) and self.datamap == other.datamap:
-            return True
-        return False
-
-    def __ne__(self, other: 'DataSet') -> bool:
-        """Check if two datasets are not equal."""
-        return not self.__eq__(other)
-
-    def copy(self) -> 'DataSet':
-        """Return a copy of the dataset."""
-        return copy.deepcopy(self)
-
-    def apply(self, func: Callable[[DataItem], DataItem]) -> 'DataSet':
-        """Apply a processing operation to the dataset.
-        Args:
-            func: The processing operation (function) to apply to the dataset.
-            *args: Arguments to pass to the processing function.
-            **kwargs: Keyword arguments to pass to the processing function.
-        Returns:
-            A new dataset with the processing operation applied.
-        """
-        new_dataset = copy.deepcopy(self)
-        new_dataset.datamap = map(lambda dataitem: func(dataitem), new_dataset.datamap)
-        return new_dataset
-
-    def write_output(self, data_dir: str, info_path: str) -> None:
-        """Execute the processing operations and write the output of the dataset to a directory.
-        Args:
-            data_dir: The directory to write the data to.
-            info_path: The path to write the info table to.
-        """
-        if not os.path.exists(data_dir):
-            os.makedirs(data_dir)
-        for dataitem in self:
-            dataitem.write_to_csv(data_dir)
-        self.info_table.to_excel(info_path, index=False)
-        if not os.path.exists(data_dir):
-            os.makedirs(data_dir)
-        loading_bar = tqdm(range(len(self.info_table)))
-        out_info_table = pd.DataFrame()
-        for i, dataitem in enumerate(copy.deepcopy(self.datamap)):
-            loading_bar.update()
-            dataitem.write_to_csv(data_dir)
-            out_info_table = pd.concat([out_info_table, dataitem.info.to_frame().T], ignore_index=True)
-            out_info_table.to_excel(info_path, index=False)
-        loading_bar.close()
