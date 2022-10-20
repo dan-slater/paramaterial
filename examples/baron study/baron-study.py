1ed0a9abc2a886e8ec3aabe7832aad221563f0ce
dan-slater <danielgslater+1@gmail.com>
2022-10-20 12:17:21 +0200

    Fixed repr curves filter combinations.

--- examples/baron study/baron-study.py
+++ examples/baron study/baron-study.py
@@ -11,10 +11,18 @@ def main():
     """Main function."""
 
     dataset = DataSet('data/02 processed data', 'info/02 processed info.xlsx')
-    dataset.make_representative_curves('data/03 repr data', 'info/03 repr info.xlsx',
-                                       ['material', 'rate', 'temperature'],
-                                       interp_res=100,
-                                       interp_by='Strain')
+
+    def drop_cols(dataitem: DataItem):
+        dataitem.data = dataitem.data.drop(columns=[
+            'Time(sec)', 'Force(kN)', 'Jaw(mm)', 'TC1(C)', 'time diff', 'Pressure(MPa)'
+        ])
+        return dataitem
+
+    dataset = dataset.apply_function(drop_cols)
+    dataset.make_representative_curves(
+        'data/03 repr data', 'info/03 repr info.xlsx',
+        ['material', 'rate', 'temperature'],
+        interp_res=100, interp_by='Strain')
 
 
 if __name__ == '__main__':
