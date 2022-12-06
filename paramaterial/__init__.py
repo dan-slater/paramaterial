90c1543d14a4eb73a8108ac09e0180c6c7f2467b
dan-slater <danielgslater+1@gmail.com>
2022-12-06 17:58:15 +0200

    Got zener_holloman plotting function working relatively well. Ready to merge.

--- paramaterial/__init__.py
+++ paramaterial/__init__.py
@@ -1,9 +1,10 @@
-from paramaterial.preparing import *
+from paramaterial import example, models
 from paramaterial.plug import DataItem, DataSet
+from paramaterial.preparing import *
 from paramaterial.processing import *
 from paramaterial.modelling import *
 from paramaterial.plotting import *
-from paramaterial.screening import make_screening_pdf, read_screening_pdf_fields
-from paramaterial import example, models
+from paramaterial.screening import *
+from paramaterial.zener_holloman import *
 
 
--- paramaterial/modelling.py
+++ paramaterial/modelling.py
@@ -15,25 +15,7 @@ from tqdm import tqdm
 from paramaterial.plug import DataItem, DataSet
 
 
-def apply_ZH_regression(ds: DataSet, flow_stress_key: str = 'flow_stress_MPa', ZH_key: str = 'ZH_parameter'):
-    """Do a linear regression for LnZ vs flow stress. #todo link
 
-    Args:
-        ds: DataSet to be fitted.
-        flow_stress_key: Info key for the flow stress value.
-        ZH_key: Info key for the ZH parameter value.
-
-    Returns:
-        The DataSet with the Zener-Holloman parameter and regression parameters added to the info table.
-    """
-    assert flow_stress_key in ds.info_table.columns, f'flow_stress_key {flow_stress_key} not in info table'
-    info_table = ds.info_table.copy()
-    info_table['lnZ'] = np.log(info_table[ZH_key].values.astype(np.float64))
-    m, c = op.curve_fit(lambda x, m, c: m * x + c, info_table['lnZ'], info_table[flow_stress_key])[0]
-    info_table['lnZ_fit_m'] = m
-    info_table['lnZ_fit_c'] = c
-    ds.info_table = info_table
-    return ds
 
 
 def make_representative_data(
