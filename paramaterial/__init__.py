eccc48cc292d7fcf15175b32650470027895ffd1
dan-slater <danielgslater+1@gmail.com>
2023-07-20 17:06:55 +0200

    making major changes to plug. finished example

--- paramaterial/__init__.py
+++ paramaterial/__init__.py
@@ -3,9 +3,8 @@ from paramaterial import example, models
 from paramaterial.plug import DataItem, DataSet
 from paramaterial.preparing import *
 from paramaterial.processing import *
-from paramaterial.modelling import *
+from paramaterial.models import *
 from paramaterial.plotting import *
 from paramaterial.screening import *
 from paramaterial.receipts import TestReceipts
-from paramaterial.zener_holloman import *
 
