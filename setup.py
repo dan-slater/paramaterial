2fcb6957f3592a98e2169a537908a062fb3ea33c
dan-slater <danielgslater+1@gmail.com>
2023-08-31 21:27:18 +0200

    Updated to version 0.1.2

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = 'Software toolkit for parameterising materials test data. ' \
               'Easily batch process experimental measurements to determine mechanical properties and material model ' \
               'parameters.'
