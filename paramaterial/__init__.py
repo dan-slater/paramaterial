26687767499faf2cf897ecc3e6d9aad2e03f65fa
dan-slater <danielgslater+1@gmail.com>
2023-08-31 21:24:05 +0200

    Updated to version 0.1.2

--- paramaterial/__init__.py
+++ paramaterial/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 from paramaterial import example, models
 from paramaterial.example import download_example
 from paramaterial.plug import DataItem, DataSet
